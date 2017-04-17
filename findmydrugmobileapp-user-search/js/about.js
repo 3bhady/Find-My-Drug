f96535c7b85cf7f02dfa47250a1dc69fed5dcaf5
3bhady <mohamed_abdelhady96@hotmail.com>
2017-04-17 19:29:23 +0200

    =

--- findmydrugmobileapp-user-search/js/about.js
+++
@@ -1,6 +0,0 @@
-myApp.onPageInit('about', function (page) {
-    myApp.alert("xx","lol");
-  /*  $.get( "http://localhost/IEEE-CUSB-Website/public/api/v1/events").done(function(data){
-      document.write(data[0].name);
-    });*/
-})
