17fa927022290f1f80432ff494f22c5dadb1b06e
Ahmed <asamir97@hotmail.com>
2017-05-19 19:38:43 +0200

    Added pharmacies response in pending requests list

--- findmydrugmobileapp-user-search/js/socket.js
+++ findmydrugmobileapp-user-search/js/socket.js
@@ -17,7 +17,50 @@ socket.on('pharmacyResponse',function(message){
     myApp.addNotification({
         message: 'pharmacy: '+message["pharmacy_name"]+' has drug '+message["drug_name"]
     });
+    CreatePharmaciesList(message);
 })
 };
 
+
+CreatePharmaciesList= function(data) {
+    var middle = '';
+    //var start = '<div class="list-block">' + ' <ul>';
+    //var end = ' </ul> ' + '</div>';
+    // for (var i = 0; i < data.length; i++) {
+    // middleList+=" <li value="+value+">"+key+"</li>";
+
+    /*        middle+='  <a id="'+value+'" href="drug.html" class="item-link"> ' +
+     '<div class="item-media"><i class="icon icon-f7"></i></div> ' +
+     '<div class="item-inner"> ' +
+     '<div class="item-title">' +
+     key+
+     '</div> ' +
+     '</div>';
+     });*/
+    middle += '    <li> ' +
+        '<a href="pharmacy.html" class="item-link item-content pharmacy" data-id="' + data[Object.keys(data)[0]] + '"> ' +
+        '<div class="item-media"><i class="icon icon-f7"></i></div> ' +
+        '<div class="item-inner"> ' +
+        '<div class="item-title">' + data[Object.keys(data)[1]] + '</div> ' +
+        '<div class="item-after">View</div> ' +
+        '</div> ' +
+        '</a> ' +
+        '</li>';
+    // }
+
+    //$$('#pharmacies-view').empty();
+    $$('#pharmacies-view').prepend( middle );
+    //$$('#pharmacies-view').append(start + middle + end);
+    //$$('#list-view').clear();
+    //$$('#list-view').append(start+middle+end);
+    $$('.pharmacy').on('click', function () {
+        localStorage.setItem("pharmacy_id", $$(this).data("id"));
+    });
+    /* $$.each(data,function(key,value) {
+     var li = document.getElementById(value).onclick = function () {
+     console.log(value);
+     localStorage.setItem("drug_id",value);
+     }
+     });*/
+};
 
